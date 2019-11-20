dbd4f82945f14fd8a8ff7eca5e3d68fb12e04f64
GerardoRodvel <jose_gerardo_lpz@hotmail.com>
2019-11-20 10:41:08 -0600

    server

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/RFID_CLIENT>'));
+app.use(express.static('./dist/RFID'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join('./dist/<RFID_CLIENT>/index.html'));
+res.sendFile(path.join('./dist/RFID/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
